0cec48ea2bb608df57988c2b41d66d000160c5e0
lucasribeiiroo <lucasribeiiroo@hotmail.com>
2024-10-02 09:13:34 -0300

    Feat: Setup inicial, header e landing page

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/header";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Schedulrr",
+  description: "Metting Scheduling App",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+            {children}
+          </main>
+          <footer className="bg-blue-100 p-8">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>@Copywright 2024</p>
+            </div>
+          </footer>
+          {/* Footer */}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
